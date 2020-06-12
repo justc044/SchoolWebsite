1aee20f78c4d3ab3ad6eae027efed985de0bf304
root <root@ip-172-31-2-114.ap-northeast-2.compute.internal>
2020-06-12 08:52:55 +0000

    changed wsgi

--- SchoolWebsite/SchoolWebsite/wsgi.py
+++ SchoolWebsite/SchoolWebsite/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.development'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SchoolWebsite.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'SchoolWebsite.settings'
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SchoolWebsite')
 
 application = get_wsgi_application()
